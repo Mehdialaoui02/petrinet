32d057967850c49e6ee633308230e4825d380562
Naoufal BENKMIL <naoufal.benkmil@imt-atlantique.net>
2023-11-07 11:00:57 +0100

    ArcTest

---
+++ src/mehdi/Zero_ArcTest.java
@@ -0,0 +1,14 @@
+package mehdi;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Zero_ArcTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
