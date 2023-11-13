88c7521da04238e622adf96d4c4a344af8cca983
Mehdialaoui02 <mehdi2002alaoui@gmail.com>
2023-11-13 14:45:45 +0100

    fix package-info

--- src/mehdi/package-info.java
+++ src/mehdi/package-info.java
@@ -1,6 +1,11 @@
 /**
- * Package principal contenant le code source du projet PetriNet.
- * Ce package inclut des classes pour modéliser un réseau de Petri, des transitions, des places,
- * et divers types d'arcs.
+ * Package principal : Contient le code source essentiel du projet PetriNet.
+ * Cette section regroupe diverses classes spécialisées dans la modélisation détaillée
+ * d'un réseau de Petri, englobant les éléments clés tels que les transitions, les places
+ * et plusieurs variantes d'arcs.
+ *
+ * La structure de ce package est  planifiée pour permettre une modélisation
+ * flexible et extensible des réseaux de Petri. Chaque classe est conçue
+ * pour capturer les relations complexes du réseaux de Petri. 
  */
 package mehdi;
